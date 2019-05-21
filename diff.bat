ba821ec776a8855d86b112a576630fcc2938a116
Pranav Singh <psingh146@sapient.com>
2019-05-21 10:28:28 +0530

    Added Comment, taking parameter from passed value for batch file, uniform small case for command in batch

--- diff.bat
+++ diff.bat
@@ -1,14 +1,16 @@
 @echo off
 
-SET CURRENT_DIR=%cd%
-SET PROJECT_DIR=%CURRENT_DIR%\..\CoreWebAppDocker
-SET BRANCH_NAME="develop"
+set CURRENT_DIR=%cd%
+set PROJECT_DIR=%CURRENT_DIR%\..\CoreWebAppDocker
+:: set BRANCH_NAME="develop"
+set BRANCH_NAME=%1
 
 echo CURRENT_DIR=%CURRENT_DIR%
-echo CURRENT_DIR=%PROJECT_DIR%
+echo PROJECT_DIR=%PROJECT_DIR%
+echo BRANCH_NAME=%BRANCH_NAME%
 
 call "C:/Program Files/git/bin/git.exe" merge-base master  %BRANCH_NAME% > merge.tmp
-SET /p MERGE_BASE=<merge.tmp
+set /p MERGE_BASE=<merge.tmp
 echo %MERGE_BASE%
 
 call "C:/Program Files/git/bin/git.exe" diff --name-only %BRANCH_NAME% %MERGE_BASE% >Delta.diff
